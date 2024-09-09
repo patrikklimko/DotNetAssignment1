41f4e160308baf3209e6d5f9c51ce2da7f3b6806
patrikklimko <patrik.klimko123@gmail.com>
2024-09-09 11:31:23 +0200

    .

--- Entities/User.cs
+++ Entities/User.cs
@@ -4,11 +4,13 @@ public class User
 {
     public string Name {get; set;}
     public int UserId {get; set;}
+    public string Password {get; set;}
 
-    public User(string name, int userId)
+    public User(string name, int userId, string password)
     {
         UserId = userId;
         Name = name;
+        Password = password;
     }
     
 }
