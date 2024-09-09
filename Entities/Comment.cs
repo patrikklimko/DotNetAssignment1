06b4a8975ced72a0e757cb3d7f6a6c57d0e78256
patrikklimko <patrik.klimko123@gmail.com>
2024-09-09 11:38:01 +0200

    .

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -5,12 +5,14 @@ public class Comment
     public int CommentId { get; set; }
     public string Text { get; set; }
     public User UserId { get; set; }
+    public Post PostId { get; set; }
 
-    public Comment(int commentId, string text, User userId)
+    public Comment(int commentId, string text, User userId, Post postId)
     {
         CommentId = commentId;
         Text = text;
         UserId = userId;
+        PostId = postId;
     }
     
 
